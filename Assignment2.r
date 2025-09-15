f9f59c01e84ec60fb820dff9288a2fba8c662cb7
Rica Gregorio <ricamaegregorio10@gmail.com>
2025-09-15 20:20:46 +0800

    Assignment #2

--- Assignment2.r
+++ Assignment2.r
@@ -1,15 +1,13 @@
 grade <- 72
 
 if (grade >= 90 && grade <= 100) {
-  print("Grade: A")
+    print("Grade: A")
 } else if (grade >= 80 && grade <= 89) {
-  print("Grade: B")
+    print("Grade: B")
 } else if (grade >= 70 && grade <= 79) {
-  print("Grade: C") 
+    print("Grade: C") 
 } else if (grade >= 60 && grade <= 69) {
-  print("Grade: D")
-} else if (grade < 60) {
-  print("Grade: F")
+    print("Grade: D")
+} else {
+    print("Grade: F")
 }
-
-
